d115904237309ad7ec20f71a9a7ac8726d3111dd
kimaiga <nelsonkimaiga@aol.com>
2016-01-18 18:11:39 +0300

    Node mailer code

--- Server.js
+++ Server.js
@@ -1,8 +1,15 @@
 var express=require('express');
 var nodemailer = require("nodemailer");
 var app=express();
+var smtpTransport = nodemailer.createTransport("SMTP",{
+service: "Gmail",
+auth: {
+user: "nelson@moringaschool.com",
+pass: "303seminarian"
+}
+});
 app.get('/',function(req,res){
-res.sendfile('index.html');
+// res.sendfile('index.html');
 });
 app.listen(3000,function(){
 console.log("Express Started on Port 3000");
